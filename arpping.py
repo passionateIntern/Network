b4f8f73f3d12f236b990d67ca123a309d4890ef8
passionateIntern <58638397+passionateIntern@users.noreply.github.com>
2022-02-14 13:16:39 -0500

    Create arpping.py

---
+++ arpping.py
@@ -0,0 +1,15 @@
+# Simple Scapy script to ARP ping
+
+from scapy.all import srp,Ether,ARP,conf
+# or from scapy.all import *
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: arpping.py <network>")
+    print("e.g.: arpping 10.10.10.0/24")
+    sys.exit(1)
+
+conf.verb=0
+ans,unans= srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=sys.argv[1]),timeout=2)
+for s,r in ans:
+    print(r.sprintf("%Ether.src% %ARP.psrc%")) 
